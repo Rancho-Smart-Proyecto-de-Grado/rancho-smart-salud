614bbf4be34ae180c9897ed2e196759d88698377
el-sebas-galeano <jsebastian-galeano@javeriana.edu.co>
2024-10-12 04:49:10 -0500

    :sparkles: feat: Creación de la Aplicación SpringBoot.

---
+++ src/main/java/com/rancho_smart/salud_composer/SaludComposerApplication.java
@@ -0,0 +1,13 @@
+package com.rancho_smart.salud_composer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaludComposerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaludComposerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rancho_smart/salud_composer/SaludComposerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rancho_smart.salud_composer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaludComposerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
